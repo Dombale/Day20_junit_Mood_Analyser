ae8e032d3f1b6afff11198f69f0883d6a9b9633b
Priyanka <priyankadombale27@gmail.com>
2022-03-20 14:14:36 +0530

    Added: message in constructor logic

--- src/test/java/com/Day20_MoodAnalyser/MoodAnalyzerTest.java
+++ src/test/java/com/Day20_MoodAnalyser/MoodAnalyzerTest.java
@@ -4,10 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+
 	@Test
 	public void givenMessage_SadMood_ShouldReturnSad() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String mood = moodAnalyzer.analyseMood("I am in Sad Mood");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am In a Sad Mood");
+		String mood = moodAnalyzer.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
 }
